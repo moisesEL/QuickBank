4304fc40ac32741819bad32a3609fd187c55c37d
moisesEL <moisesel97@gmail.com>
2025-11-25 12:35:21 +0100

    ejercicios

--- web/src/util/change_password.js
+++ web/src/util/change_password.js
@@ -22,4 +22,36 @@ async function changePassword(newPassword) {
     })
 }
 
-export default changePassword;
+ export default changePassword;
+
+
+ 
+// async function changePassword(newPassword) {
+//     const valueNewPassword = newPassword.value.trim();
+//     const xml = {
+//             <customer>
+//                 <id>${id}</id>
+//                 <firstName>${firstName}</firstName>
+//                 <lastName>${lastName}</lastName>
+//                 <middleInitial>${middleInitial}</middleInitial>
+//                 <email>${email}</email>
+//                 <password>${password}</password>
+//                 <phone></phone>
+//                 <street>${street}</street>
+//                 <city>${city}</city>
+//                 <state>${state}</state>
+//                 <zip><${zip}/zip>            
+//             </customer>
+
+//     }
+
+//     return fetch("/CRUDBankServerSide/webresources/customer/", {
+//         method: "PUT",
+//         headers: {
+//             "Content-Type": "application/xml"
+//         },
+//         body: xml
+//     })
+// }
+
+// export default changePassword;
