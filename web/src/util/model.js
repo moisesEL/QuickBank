0cfff715dc99d514041e07d83ffc0fc48c21def8
moisesEL <moisesel97@gmail.com>
2026-01-20 10:58:08 +0100

    parte inicial

---
+++ web/src/util/model.js
@@ -0,0 +1,13 @@
+class movement {
+    constructor(id, balance, amount, description, timestamp) {
+        this.id = id;
+        this.balance = balance;
+        this.amount = amount;
+        this.description = description;
+        this.timestamp = timestamp;
+        
+
+    }
+
+
+}
