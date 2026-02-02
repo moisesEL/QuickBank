de91ffcf4617edb827ab75b08631ae2431aba081
aitortita <aitor_ezcu@hotmail.com>
2026-02-02 13:31:51 +0100

    PUT working without validations

--- web/src/util/accounts.js
+++ web/src/util/accounts.js
@@ -4,22 +4,29 @@ import { Account } from "./model.js";
 let customerId = sessionStorage.getItem('customer.id')
 
 window.addEventListener("DOMContentLoaded", () => {
-
-    // Server petition to bring all user's accounts
-    fetch_accounts_by_user_id(customerId)
-    .then(accounts => {
-        // Extracting tableBody from DOM
-        const tableBody = document.querySelector("#tableBody");
-        // Generating each account row using a generator function
-        const rowGenerator = account_row_generator(accounts);
-        // Append each row to the tableBody
-        for (const row of rowGenerator) {
-            tableBody.appendChild(row);
-        }
-    })
-    .catch(error => {
+    try {
+        // Server petition to bring all user's accounts
+        fetch_accounts_by_user_id(customerId)
+        .then(accounts => {
+            // Extracting tableBody from DOM
+            const tableBody = document.querySelector("#tableBody");
+            // Generating each account row using a generator function
+            const rowGenerator = account_row_generator(accounts);
+            // Append each row to the tableBody
+            for (const row of rowGenerator) {
+                tableBody.appendChild(row);
+            }
+        })
+        .then(() => {
+            const addAccount = document.createElement("button");
+            addAccount.innerText = "add account";
+            // Extracting tableBody from DOM
+            const tableBody = document.querySelector("#tableBody");
+            tableBody.appendChild(addAccount)
+        })
+    } catch (error) {
         console.log(error.message);
-    })
+    }
 })
 
 
@@ -52,17 +59,25 @@ function* account_row_generator(accounts) {
     ["id", "type", "description", "balance", "creditLine", "beginBalance", "beginBalanceTimestamp"].forEach(field => {
         const cellContainer = document.createElement("div");
         cellContainer.setAttribute("role", "cell container");
-
-        const cell = document.createElement("p");
-        cell.setAttribute("role", "cell");
-        cell.setAttribute("data-role", field);
-        cell.innerText = account[field];
-        if (field !== "id" && field !== "beginBalance" && field !== "beginBalanceTimestamp") {
-            cell.addEventListener("dblclick", handleCellDoubleClick)
+        if (field === "id") {
+            const cell = document.createElement("a");
+            cell.setAttribute("href", `/QuickBank/src/views/movements.html?account=${account[field]}`)
+            cell.setAttribute("data-role", field);
+            cell.innerText = account[field];
+            cellContainer.appendChild(cell);
+            row.appendChild(cellContainer);
         }
-
-        cellContainer.appendChild(cell);
-        row.appendChild(cellContainer);
+        else {
+            const cell = document.createElement("p");
+            cell.setAttribute("role", "cell");
+            cell.setAttribute("data-role", field);
+            cell.innerText = account[field];
+            if (field !== "id" && field !== "beginBalance" && field !== "beginBalanceTimestamp") {
+                cell.addEventListener("dblclick", handleCellDoubleClick)
+            }
+            cellContainer.appendChild(cell);
+            row.appendChild(cellContainer);
+        }
     });
     yield row;
     }
@@ -70,59 +85,82 @@ function* account_row_generator(accounts) {
 
 function handleCellDoubleClick(event) {
     const cell = event.currentTarget;
-        // Get the current value
-        const originalValue = cell.innerText;
-        const cellContainer = cell.parentNode;
-        const row = cellContainer.parentNode;
+    // Get the original cell value
+    const originalValue = cell.innerText;
 
-        console.log({originalValue})
-        console.log({cellContainer})
-        
-        // Create input element
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.value = originalValue;
-        input.className = 'edit-input';
-        
-        // Replace the <p> with the <input>
-        cellContainer.replaceChild(input, cell);
-        
-        // Focus the input
-        input.focus();
-        input.select();
-        
-        // Add event listeners for saving on Enter or losing focus
-        input.addEventListener('keydown', (event) => {
-            if (event.key === 'Enter') {
-                saveCellChanges(input, originalValue);
-            }
-            if (event.key === 'Escape') {
-                cancelEdit(input, originalValue);
-            }
-        });
-        
-        input.addEventListener('blur', () => {
+    // Get parent container
+    const cellContainer = cell.parentNode;
+
+    // Create input element
+    const input = document.createElement('input');
+    input.setAttribute("data-role", cell.getAttribute("data-role"))
+    input.type = 'text';
+    input.value = originalValue;
+    input.className = 'edit-input';
+    
+    // Replace the <p> with the <input>
+    cellContainer.replaceChild(input, cell);
+    
+    // Focus the input
+    input.focus();
+    input.select();
+    
+    // Add event listeners for saving on Enter or losing focus
+    input.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            saveCellChanges(input, originalValue);
+        }
+        if (event.key === 'Escape') {
             cancelEdit(input, originalValue);
-        });
+        }
+    });
+    
+    input.addEventListener('blur', () => {
+        cancelEdit(input, originalValue);
+    });
 }
 
 function saveCellChanges(input, originalValue) {
+    // Extract new value from input
     const newValue = input.value;
+    // get cell parent
     const cellContainer = input.parentNode;
+    // Get parent row
+    const row = cellContainer.parentNode;
+
+    // Create new account object to store data and send on fetch body
+    const account = new Account ();
+
+    // Iterate on each column of the row
+    row.childNodes.forEach((div) => {
+        // Get cell element from column container
+        let cellElement = div.childNodes[0];
+        // If the column container is the same than the one we are editing
+        // then use the value from the input
+        if (div.isSameNode(cellContainer))
+            account[cellElement.getAttribute("data-role")] = newValue
+        // If the column container is not the same than the one we
+        // are editing, then simply extract the element's innerText
+        else account[cellElement.getAttribute("data-role")] = cellElement.innerText
+    })
 
     // TODO: Call to server
-    fetch(`/CRUDBankServerSide/webresources/account/customer/${encodeURIComponent(customerId)}`, {
+    // GET /CRUDBankServerSide/webresources/account/customer/:idCustomer
+    // GET /CRUDBankServerSide/webresources/account/:accountId
+    // PUT /CRUDBankServerSide/webresources/account
+    // DELETE /CRUDBankServerSide/webresources/account
+    fetch('/CRUDBankServerSide/webresources/account', {
         method: 'PUT',
         headers: {
-            'Accept': 'application/json'
-        }
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(account)
     })
     .then(response => {
         console.log(response.status)
-    })
-    .then(() => {
         // Create new p element with the updated value
         const p = document.createElement('p');
+        p.setAttribute("data-role", input.getAttribute("data-role"))
         p.textContent = newValue;
         
         // Replace input with p
@@ -133,9 +171,7 @@ function saveCellChanges(input, originalValue) {
     })
     .catch(error => {
         console.log(error.message)
-        cancelEdit(input, originalValue)
-        // Re-attach the double-click listener
-        p.addEventListener('dblclick', handleCellDoubleClick);
+        cancelEdit(input, originalValue);
     })
 
 }
@@ -145,7 +181,8 @@ function cancelEdit(input, originalValue) {
     
     // Create new p element with the original value
     const p = document.createElement('p');
-    p.textContent = originalValue;
+    p.setAttribute("data-role", input.getAttribute("data-role"))
+    p.innerText = originalValue;
     
     // Replace input with p
     cellContainer.replaceChild(p, input);
